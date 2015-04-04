db037462415ab55d84b7da3ae03e4ed12a8a0110
Justin R. Miller <incanus@codesorcery.net>
2015-04-03 17:42:13 -0700

    build all, then run four sets of tests

--- scripts/test_ios.sh
+++ scripts/test_ios.sh
@@ -4,11 +4,36 @@ set -e
 set -o pipefail
 set -u
 
-xctool test \
+xctool \
+    -project ./test/ios/ios-tests.xcodeproj \
+    -scheme 'Mapbox GL Tests' \
+    -sdk iphonesimulator \
+    build-tests
+
+xctool \
     -project ./test/ios/ios-tests.xcodeproj \
     -scheme 'Mapbox GL Tests' \
     -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 5s,OS=7.1' \
+    test
+
+xctool \
+    -project ./test/ios/ios-tests.xcodeproj \
+    -scheme 'Mapbox GL Tests' \
+    -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.1' \
+    test
+
+xctool \
+    -project ./test/ios/ios-tests.xcodeproj \
+    -scheme 'Mapbox GL Tests' \
+    -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPad 2,OS=7.1' \
+    test
+
+xctool \
+    -project ./test/ios/ios-tests.xcodeproj \
+    -scheme 'Mapbox GL Tests' \
+    -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPad 2,OS=8.1'
+    test
