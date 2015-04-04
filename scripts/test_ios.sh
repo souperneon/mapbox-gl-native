62d780103b4bc6fe78bd754f7fb9141ead1a0dd6
Justin R. Miller <incanus@codesorcery.net>
2015-04-03 18:47:31 -0700

    xctool -> xcodebuild/xcpretty to allow multiple tests at once

--- scripts/test_ios.sh
+++ scripts/test_ios.sh
@@ -4,36 +4,12 @@ set -e
 set -o pipefail
 set -u
 
-xctool \
-    -project ./test/ios/ios-tests.xcodeproj \
-    -scheme 'Mapbox GL Tests' \
-    -sdk iphonesimulator \
-    build-tests
-
-xctool \
+xcodebuild test \
     -project ./test/ios/ios-tests.xcodeproj \
     -scheme 'Mapbox GL Tests' \
     -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 5s,OS=7.1' \
-    test
-
-xctool \
-    -project ./test/ios/ios-tests.xcodeproj \
-    -scheme 'Mapbox GL Tests' \
-    -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPhone 5s,OS=8.1' \
-    test
-
-xctool \
-    -project ./test/ios/ios-tests.xcodeproj \
-    -scheme 'Mapbox GL Tests' \
-    -sdk iphonesimulator \
     -destination 'platform=iOS Simulator,name=iPad 2,OS=7.1' \
-    test
-
-xctool \
-    -project ./test/ios/ios-tests.xcodeproj \
-    -scheme 'Mapbox GL Tests' \
-    -sdk iphonesimulator \
-    -destination 'platform=iOS Simulator,name=iPad 2,OS=8.1'
-    test
+    -destination 'platform=iOS Simulator,name=iPad 2,OS=8.1' \
+    | xcpretty -c
